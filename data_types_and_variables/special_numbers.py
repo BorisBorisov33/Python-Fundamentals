cccaf08ede406e4567c39b8741c094c8321705a2
BorisBorisov33 <bobbyborisov16@gmail.com>
2022-06-07 09:43:16 +0200

    special numbers exercise update

--- data_types_and_variables/special_numbers.py
+++ data_types_and_variables/special_numbers.py
@@ -1,24 +1,7 @@
 number = int(input())
-
-sum_digits = 0
-# print(number[0])
 for number in range(1, number + 1):
-    number = str(number)
-    for digit in number:
-        if len(number) > 1:
-            sum_digits += int(digit)
-            if sum_digits% 5 ==0 or sum_digits% 11==0 or sum_digits % 7 ==0:
-                print(f"{number} -> True")
-            # else:
-                # print(f"{number} -> False")
-
-        else:
-            if int(number) % 5 == 0:
-                print(f"{number} -> True")
-            elif int(number) % 7 == 0:
-                print(f"{number} -> True")
-            elif int(number) % 11 == 0:
-                print(f"{number} -> True")
-            else:
-                print(f"{number} -> False")
+    if number % 10 + number // 10 == 5 or number % 10 + number // 10 == 7 or number % 10 + number // 10 == 11:
+        print(f"{number} -> True")
+    else:
+        print(f"{number} -> False")
 
